e398365b04bd37b1e6f8c89d0638c8faa503194e
Daniil Anekov <daniilanekov@mail.ru>
2023-05-20 20:51:51 +0300

    Added image for db postgres.

--- src/main/java/com/cryptocurrencyalert/repisitories/ImagesRepository.java
+++ src/main/java/com/cryptocurrencyalert/repisitories/ImagesRepository.java
@@ -2,6 +2,8 @@ package com.cryptocurrencyalert.repisitories;
 
 import com.cryptocurrencyalert.models.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImagesRepository extends JpaRepository<Image,Integer> {
 }
--- src/main/java/com/cryptocurrencyalert/repisitories/PeopleRepository.java
+++ src/main/java/com/cryptocurrencyalert/repisitories/PeopleRepository.java
@@ -2,6 +2,8 @@ package com.cryptocurrencyalert.repisitories;
 
 import com.cryptocurrencyalert.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PeopleRepository extends JpaRepository<Person,Integer> {
 }
